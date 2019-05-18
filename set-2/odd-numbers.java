360868146009e5e1c2f276149bd3fcb319fc7e2d
nikhilfurious <aashishchandak1999@gmail.com>
2019-05-18 12:19:12 +0530

    Update odd-numbers.java

--- set-2/odd-numbers.java
+++ set-2/odd-numbers.java
@@ -5,7 +5,7 @@ class Odd{
     {
         for(int i=strating_number;i<ending_number;i++)
         {
-            if(i%2 != 0)
+            if(i%2 != 0 && i!=1)
             {
                 System.out.println(i);
             }
