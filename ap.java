2c19060c14a08a37108066698956fc4311476c3f
nikhilfurious <aashishchandak1999@gmail.com>
2019-07-06 11:16:47 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+class Main{ 
+
+	static float sumOfAP(float a, float d, 
+								int n) 
+	{ 
+		float sum = 0; 
+		for (int i = 0; i < n; i++) 
+		{ 
+			sum = sum + a; 
+			a = a + d; 
+		} 
+		return sum; 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		float a = sc.nextFloat(),d = sc.nextFloat(); 
+		System.out.println(sumOfAP(a, d, n)); 
+	} 
+} 
+
+
