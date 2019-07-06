2188f463e523f14e69fce882cd757f4a77e0f0dd
nikhilfurious <aashishchandak1999@gmail.com>
2019-07-06 11:37:53 +0530

    Update ap.java

--- ap.java
+++ ap.java
@@ -1,24 +1,25 @@
-
 import java.util.Scanner;
 public class Main{ 
 
-	static float sumOfAP(float a, float d,int n) 
+	void check(int a, int d,int n) 
 	{ 
-		float sum = 0; 
+		int sum = 0; 
 		for (int i = 0; i < n; i++) 
 		{ 
 			sum = sum + a; 
 			a = a + d; 
 		} 
-		return sum; 
+		System.out.println(sum);
+	    
 	} 
 
 	public static void main(String args[]) 
 	{ 
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		float a = sc.nextFloat(),d = sc.nextFloat(); 
-		System.out.println(sumOfAP(a, d, n)); 
+		int a = sc.nextInt();
+		int d = sc.nextInt(); 
+	   check(a, d, n); 
 	} 
 } 
 
